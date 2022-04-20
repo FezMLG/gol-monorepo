3aa4ed57d01ada0318cf601655558a1df2707160
FezMLG <46359181+FezMLG@users.noreply.github.com>
2022-04-20 10:12:28 +0200

    adding game of life codebase

--- packages/game-of-life/src/lib/game-of-life.spec.ts
+++
@@ -1,7 +0,0 @@
-import { gameOfLife } from './game-of-life';
-
-describe('gameOfLife', () => {
-  it('should work', () => {
-    expect(gameOfLife()).toEqual('game-of-life');
-  });
-});
--- packages/game-of-life/src/lib/game-of-life.ts
+++
@@ -1,3 +0,0 @@
-export function gameOfLife(): string {
-  return 'game-of-life';
-}
